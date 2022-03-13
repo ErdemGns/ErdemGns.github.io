a129adf1b17b4565bd362e74044d881c7bec1911
Erdem <gnserdem13@gmail.com>
2022-03-13 14:13:21 +0300

    added new file

---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+    Ödev:
+        OpenStreetMap üzerinden harita altlığını çek, 
+        daha sonra site üzerinden belirli yerlerin konumu göster.
+        Leaflet OSM üzerinden veriler çekilecek ve  icon eklenecek.
+*/
+
+/*
+    Koordinat sistemi:
+        WGS 84 Coğrafi koordinatlar olacak.
+        (Longitude) Lon: 35.252
+        (Latitude) Lat: 39.031 
+        Zoom: 7
+        EPGS: "EPSG:4326"
+*/
+
+/*
+    Türkiye veya Dünya gösterilecek konumlar:
+        Belli bir konu seçip Dünya veya Türkiye 
+        harita altlığı üzerinden yerlerin konummunu göster.
+*/
+
+/*
+    Gösterilecek konum örnekler:
+        - Dünya ülke başkent konum veriler
+        - Türkiye şehir merkezleri konum verileri
+        - 
+*/
+
+
+map = new OpenLayers.Map("mapdiv");
+map.addLayer(new OpenLayers.Layer.OSM());
+
+// transform from WGS 1984 
+// to Spherical Mercator Projection
+var lonLat = new OpenLayers.LonLat( 35.252, 39.031 )
+.transform(
+    new OpenLayers.Projection("EPSG:4326"), 
+    map.getProjectionObject()
+);
+    
+var zoom = 7;
+
+var markers = new OpenLayers.Layer.Markers( "Markers" );
+map.addLayer(markers);
+
+//var test = new OpenLayers.lonLat(37.252, 39.031)
+
+markers.addMarker(new OpenLayers.Marker(lonLat));
+
+map.setCenter (lonLat, zoom);
+
